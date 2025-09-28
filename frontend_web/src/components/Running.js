885e776f9b186ebf381a237f28e62541c79c98d9
BIMAL P.R. KUJUR <33995620+kujurbimal@users.noreply.github.com>
2025-09-28 14:43:05 +0530

    Add files via upload

---
+++ frontend_web/src/components/Running.js
@@ -0,0 +1,12 @@
+import React from "react"; 
+
+const Running = () => { 
+ return ( 
+  <div style={{ padding: "20px" }}> 
+   <h2>Running & Cycling</h2> 
+   <p>Log your workouts, measure distance, and compete with friends.</p> 
+  </div> 
+ ); 
+}; 
+
+export default Running;
